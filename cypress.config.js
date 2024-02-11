1933c158cc30655da1f278695b82ce2c12eb13b6
Mike McCready <66998419+MikeMcC399@users.noreply.github.com>
2024-02-11 17:37:35 +0100

    Add Cypress Cloud project ID

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "x8m6og",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
